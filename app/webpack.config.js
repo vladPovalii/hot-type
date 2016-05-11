e9ffab1c51f6b23625e21740c2a090f4204ac115
Vlad Povalii <vladpovalii@gmail.com>
2016-05-11 23:54:08 +0300

    removed node_modules

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -6,7 +6,11 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
-        ]
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.(woff|woff2)$/,  loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+            { test: /\.ttf$/,    loader: "file-loader" },
+            { test: /\.eot$/,    loader: "file-loader" },
+            { test: /\.svg$/,    loader: "file-loader" }     
+       ]
     }
 };
