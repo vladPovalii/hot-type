8c15ff73f29e9ff3ae77dd5d2f6a838019a30e6b
Vlad Povalii <vladpovalii@gmail.com>
2016-05-15 09:50:59 +0300

    upd

--- app/main.js
+++ app/main.js
@@ -3,10 +3,12 @@ require('bootstrap-webpack!./bootstrap.config.js');
 
 var form = document.querySelector("#type");
 form.addEventListener("submit", function(event) {
-	var h = document.createElement("h3");
-	var t = document.createTextNode(form.elements.input.value);
-	h.appendChild(t);
-	document.body.appendChild(h);
-	form.elements.input.value = "";
+	var type_div = document.createElement("div");
+	var type_title = document.createTextNode(form.elements.col_3.value);
+	type_div.className = "type-item";
+	type_title.className = "type-item__title";
+	type_div.appendChild(type_title);
+	document.body.appendChild(type_div);
+	form.elements.col_3.value = "";
 	event.preventDefault();
 }); 
