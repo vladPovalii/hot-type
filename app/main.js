9a168bd43415e6af624092e21f268ab8982d83fd
Vlad Povalii <vladpovalii@gmail.com>
2016-05-16 20:19:16 +0300

    upd

--- app/main.js
+++ app/main.js
@@ -2,13 +2,14 @@ require('expose?$!expose?jQuery!jquery');
 require('bootstrap-webpack!./bootstrap.config.js');
 
 var form = document.querySelector("#type");
+var list = document.querySelector(".type-list");
 form.addEventListener("submit", function(event) {
-	var type_div = document.createElement("div");
+	var type_item_div = document.createElement("div");
 	var type_title = document.createTextNode(form.elements.col_3.value);
-	type_div.className = "type-item";
+	type_item_div.className = "type-item";
 	type_title.className = "type-item__title";
-	type_div.appendChild(type_title);
-	document.body.appendChild(type_div);
+	type_item_div.appendChild(type_title);
+	list.appendChild(type_item_div);
 	form.elements.col_3.value = "";
 	event.preventDefault();
 }); 
