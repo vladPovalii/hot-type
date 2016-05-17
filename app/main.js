55f1078795795502ae675ede357807eb656613b5
Vlad Povalii <vladpovalii@gmail.com>
2016-05-17 18:52:30 +0300

    updated css and main.js code refactoring

--- app/main.js
+++ app/main.js
@@ -1,15 +1,32 @@
 require('expose?$!expose?jQuery!jquery');
 require('bootstrap-webpack!./bootstrap.config.js');
 
-var form = document.querySelector("#type");
-var list = document.querySelector(".type-list");
-form.addEventListener("submit", function(event) {
+function subscibeForSubmit(){
+	var form = document.querySelector("#type");
+	var list = document.querySelector(".type-list");
+	form.addEventListener("submit", function(event) {
+		if(form.elements.col_3.value){
+			createTypeItem(form, list);
+		}else{
+			event.preventDefault();
+		}
+	}); 
+}
+
+function createTypeItem(submited_form, parent_div) {
 	var type_item_div = document.createElement("div");
-	var type_title = document.createTextNode(form.elements.col_3.value);
-	type_item_div.className = "type-item";
-	type_title.className = "type-item__title";
-	type_item_div.appendChild(type_title);
-	list.appendChild(type_item_div);
-	form.elements.col_3.value = "";
+	var type_item_title_div = document.createElement("div");
+	var type_title = document.createTextNode(submited_form.elements.col_3.value);
+
+	type_item_div.classList.add("type-item");
+	type_item_div.classList.add("type-item--completed");
+	type_item_title_div.className = "type-item-title";
+
+	type_item_title_div.appendChild(type_title);
+	type_item_div.appendChild(type_item_title_div);
+	parent_div.appendChild(type_item_div);
+	submited_form.elements.col_3.value = "";
 	event.preventDefault();
-}); 
+}
+
+subscibeForSubmit();
